185549ac1f3c0b0e1745c909b5ac017d057180ab
Hifzhan Akraman <hifzhan@transfez.com>
2022-05-23 18:29:49 +0700

    added CRUD and created comments

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'articles#home'
 
-  resources :articles, only: [:show]
+  resources :articles, only: [:index, :show, :new, :create, :edit, :update, :destroy] do
+    resources :comments
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_04_24_233419) do
+ActiveRecord::Schema.define(version: 2022_05_23_112348) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "description"
     t.datetime "created_at"
     t.datetime "updated_at"
+  end
+
+  create_table "comments", force: :cascade do |t|
+    t.string "commenter"
+    t.text "body"
+    t.integer "article_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["article_id"], name: "index_comments_on_article_id"
   end
 
 end
